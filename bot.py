5954d2d6f5881234fcca9bfb75a57a5ee07abf9a
Joseph Jiang <josephjiang123@gmail.com>
2024-05-15 04:24:35 -0500

    "added insults and compliments"

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import os
 import asyncio
 import discord
 import birthdays
+import util
+import random
 from dotenv import load_dotenv
 
 intents = discord.Intents.default()
@@ -22,7 +24,6 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     called_once_a_day_at_midnight.start()
     channel = bot.get_channel(CHANNEL_ID)
-    await channel.send('Hello!')
 
 @bot.command()
 async def isItMyBirthday(ctx):
@@ -41,7 +42,14 @@ async def insult(ctx, member: discord.Member):
     if member.global_name == "Pidgeon":
         await ctx.send(file=discord.File('snape.jpg'))
     else:
-        await ctx.send(f'You are a smelly dog, {member.mention}')
+        insult = random.choice(util.insults)
+        print(insult)
+        await ctx.send(f'{insult}, {member.mention}')
+
+@bot.command()
+async def love(ctx, member: discord.Member):
+    compliment = random.choice(util.compliments)
+    await ctx.send(f'{compliment}, {member.mention}')
 
 
 @bot.command()
@@ -62,14 +70,13 @@ def seconds_until_midnight():
 @tasks.loop(seconds=1)
 async def called_once_a_day_at_midnight():
     message_channel = bot.get_channel(CHANNEL_ID)
-    await message_channel.send(seconds_until_midnight())
     await asyncio.sleep(seconds_until_midnight())
     birthday = birthdays.getTodaysBirthdays()
     guild = bot.get_guild(GUILD_ID)
     guild.fetch_members()
 
     for member in birthday:
-        await ctx.send(f'Happy birthday {guild.get_member_named(member).mention}!')
+        await message_channel.send(f'Happy birthday {guild.get_member_named(member).mention}!')
 
 @called_once_a_day_at_midnight.before_loop
 async def before():
---
+++ util.py
@@ -0,0 +1,10 @@
+compliments = ["Your life is EVERYTHING",
+               "You should LOVE yourself NOW",
+               "You serve ALL PURPOSE"]
+
+
+
+insults = ["You are a smelly dog",
+           "You are a smelly cat",
+           "Your mother was a hamster and your father smely of elderberries",
+           "I fart in your general direction"]
